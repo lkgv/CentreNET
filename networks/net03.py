07e63fdd788a89ee50fc482389a9a93d30fc63bf
lkgv <lkgv@foxmail.com>
2018-03-27 10:26:54 +0800

    Fixed a bug

--- networks/net03.py
+++ networks/net03.py
@@ -2,8 +2,6 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from torchvision import models
-from torch.autograd import variable
-from torch_deform_conv.layers import ConvOffset2D
 
 DEBUG = False
 
